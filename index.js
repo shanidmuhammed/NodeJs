d8062111508bc75cd68ee019c63fa961a72041d4
shanidmuhammed <159933780+shanidmuhammed@users.noreply.github.com>
2024-10-28 18:03:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.writeHead(200, { 'content-type' : 'text/html' });
-        res.end('<h1>Hello I am Shanid</h1>');
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Shanid</title>
+</head>
+<body>
     
-})
-    const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log('Listening on port 3000');
-    
-})
+</body>
+</html>
